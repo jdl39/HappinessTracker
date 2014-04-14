5f31f1d247873ef514ca603983bb664019aebf57
Jimmy Du <jimmydu@dn0a225231.sunet>
2014-04-13 18:51:39 -0700

    Initialized repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HappinessApp::Application.config.secret_key_base = '85ea2c5bc902654bb445303353ea4e4d1a4f8bdd3f8ffdc0e7d3e398f2ee54f802ecf15b4db40ba4b7262fb04a5f296e6cc6eab573e5becdb3c2bb2f2e1d5d4c'
