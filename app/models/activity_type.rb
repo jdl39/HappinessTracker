f2bc5c87018acd623ba12fb9163d0b370f471692
Jimmy Du <jimmydu@rescomp-13-208562.stanford.edu>
2014-04-24 16:53:17 -0700

    Adding features to activity type

--- app/models/activity_type.rb
+++ app/models/activity_type.rb
@@ -1,4 +1,6 @@
 class ActivityType < ActiveRecord::Base
+    before_save { self.email = email.downcase }
+	validates :name, presence: true, length: { maximum: 20 }
 	has_many :activities
 	has_many :goal_types
 end
