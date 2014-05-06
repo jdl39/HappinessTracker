77ea7130ea1f0ddf74156612385851d0902b2ab4
Jimmy Lee <jdl39@stanford.edu>
2014-05-05 19:27:50 -0700

    Added Happiness Tracking models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	has_many :activities
 	has_many :friendships
     has_many :users, :through => :friendships
+    has_many :happiness_responses
 
 	validates :username, presence: true, uniqueness: true
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
