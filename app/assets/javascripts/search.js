3763538ab4628d545b72b59e20546fb7970e43bb
Jeremy Hines <jchines@stanford.edu>
2014-05-12 17:15:29 -0700

    switching over to coffeescript

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -7,10 +7,22 @@ function search() {
     xhr.onreadystatechange = xhrHandler;
     var str = document.getElementById('search').value;
     console.log(str);
+    if(str) {
+        var panel = document.getElementById('friends-panel');
+        panel.style.visibility="visible";
+        var panel = document.getElementById('right-panel');
+        panel.style.visibility="visible";
+    } else {
+        var panel = document.getElementById('friends-panel');
+        panel.style.visibility="hidden";
+        var panel = document.getElementById('right-panel');
+        panel.style.visibility="hidden";
+    }
     var friends_header = document.getElementById('friends-header');
     friends_header.innerHTML = str;
     var graph_header = document.getElementById('graph-header');
-    graph_header.innerHTML = str;    
+    graph_header.innerHTML = str;
+// "http://localhost:3000/searchjson?str=hey"
     // xhr.open("GET", url+encodeURIComponent(str), true);
     // xhr.send();
     // function xhrHandler() {
