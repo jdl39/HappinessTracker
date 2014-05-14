bd14a697da0aaf7f0c1d850b7068bef03fffda09
Steffee <idiodabble@gmail.com>
2014-05-14 15:14:32 -0700

    added an index

--- db/migrate/20140502204017_create_activity_words.rb
+++ db/migrate/20140502204017_create_activity_words.rb
@@ -2,7 +2,7 @@ class CreateActivityWords < ActiveRecord::Migration
   def change
     create_table :activity_words do |t|
           t.references :activity_type, index: true
-          t.text :word
+          t.text :word, index: true
 
       t.timestamps
     end
