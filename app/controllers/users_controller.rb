bbfb1a29128691e164f209129e99535ef8db392d
Jimmy Du <jimmydu@dn0a22507d.sunet>
2014-05-15 16:54:23 -0700

    Added variable to access viewed_user in non_friends and friends templates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,16 +31,14 @@ class UsersController < ApplicationController
   end
 
   def profile
-     viewed_user = User.find_by_username(params[:username])
-	      if (params[:username] == nil || is_current_user(viewed_user.username))
+     @viewed_user = User.find_by_username(params[:username])
+	      if (params[:username] == nil || is_current_user(@viewed_user.username))
 		      # Render user's personal profile page
 			  render 'my_profile' 
-		  elsif (are_friends(current_user.id, viewed_user.id))
-			  @other_user_username = viewed_user.username
+		  elsif (are_friends(current_user.id, @viewed_user.id))
 			  # Show friend-permissable view
 			  render 'friend_profile'
 		  else
-			  @other_user_username = viewed_user.username
 			  # Render non-friend page
 		      render 'non_friend_profile'
 	      end
