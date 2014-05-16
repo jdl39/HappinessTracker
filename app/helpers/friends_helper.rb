bbc316bacefee3603b5afd0364edc8de2be07d3f
Jimmy Du <jimmydu@dn0a22507d.sunet>
2014-05-16 00:25:43 -0700

    Fixed logic bug in friends

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -2,11 +2,13 @@ module FriendsHelper
 
 	def get_friend_info (user_id, accepted)
         friends = []
-		friend_relationships = Friend.where(user_id: user_id, accepted: accepted)
-	    friend_relationships.each do |friend_relationship|
-            friend_found = User.find(friend_relationship.friend_id)
-			friend_found.request_id = friend_relationship.id
-			friends << friend_found
+		if accepted
+		    friend_relationships = Friend.where(user_id: user_id, accepted: accepted)
+	        friend_relationships.each do |friend_relationship|
+                friend_found = User.find(friend_relationship.friend_id)
+			    friend_found.request_id = friend_relationship.id
+			    friends << friend_found
+		    end
 		end
 		friend_relationships = Friend.where(friend_id: user_id, accepted: accepted)
 	    friend_relationships.each do |friend_relationship|
