7f8380ae6ea0e94296cf87cbb3a07f6bb9f1cdad
Jimmy Lee <jdl39@stanford.edu>
2014-05-20 17:17:54 -0700

    Reset the jquery location again. The local version causes issues.

--- app/models/goal_type.rb
+++ app/models/goal_type.rb
@@ -3,4 +3,20 @@ class GoalType < ActiveRecord::Base
   belongs_to :activity_type
   belongs_to :measurement_type
   has_many :goals
+
+  def GoalType.goal_type_for(activity_type, measurement_type, is_repeated, requires_greater, measurement_value, days_to_complete)
+  	to_return = GoalType.find_by(activity_type: activity_type, measurement_type: measurement_type, is_repeated: is_repeated, requires_greater: requires_greater, measurement_value: measurement_value, days_to_complete: days_to_complete, guide: nil)
+  	if to_return.nil?
+  		to_return = GoalType.new
+  		to_return.activity_type = activity_type
+  		to_return.measurement_type = measurement_type
+  		to_return.is_repeated = is_repeated
+  		to_return.requires_greater = requires_greater
+  		to_return.measurement_value = measurement_value
+  		to_return.days_to_complete = days_to_complete
+  		to_return.guide = nil
+  		to_return.save
+  	end
+  	return to_return
+  end
 end
