84b40c2ff5c70f29f1fc847bbe0e379c24b714d0
Jimmy Du <jimmydu@rescomp-13-208562.stanford.edu>
2014-05-30 01:08:37 -0700

    Added controller logic and view for inbox

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,20 @@
 class MessagesController < ApplicationController
+
+	include MessagesHelper
+
     # Renders page based on whether user/friend/non-friend
 	def inbox
        viewed_user = User.find_by_username(params[:username]) 
 	      if (params[:username] == nil || is_current_user(viewed_user.username))
-			  @messages = Message.where(sender_id: current_user.id)
-			  @messages += Message.where(receiver_id: current_user.id)
+			  @messages = recently_sent_messages(current_user.id)
+			  @messages += recently_received_messages(current_user.id)
+			  @messages.sort! {|a,b| b[:timestamp] <=> a[:timestamp] }
 			  #Render personal view of inbox
 			  render 'my_inbox'
 		  elsif (are_friends(current_user.id, viewed_user.id))
-			  # Renders friend-view of messages page
+			  # TODO: Renders friend-view of messages page
 			  @messages = Message.where(sender_id:current_user.id, receiver_id:friend.id)
 			  @messages += Message.where(sender_id:friend.id, receiver_id:current_user.id)
-			  @messages.sort! {|a,b| a.created_at <=> b.created_at }
 			  render 'friend_inbox'
 		  else
 			  #Render non-friend-view
--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,32 @@
 module MessagesHelper
+
+  # Recently sent challenges
+  def recently_sent_messages(user_id)
+	messages = Message.where(sender_id:user_id).recent
+	return populate_messages(messages)
+  end
+
+  # Recently received challenges
+  def recently_received_messages(user_id)
+	messages = Message.where(receiver_id:user_id).recent
+	return populate_messages(messages)
+  end 
+
+  # Populates the messages
+  def populate_messages(messages)
+	results = []
+	messages.each do |message|
+	  sender = User.find(message.sender_id) 
+	  receiver = User.find(message.receiver_id)
+	  result = {:sender => sender, 
+				:receiver => receiver, 
+				:message => message, 
+				:type => 'message',
+				:timestamp => message.created_at
+			   }   
+	  results << result
+	end 
+	return results
+  end 
+
 end
